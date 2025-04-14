ee053f13500aaf13a278a1559cfbe652bc8c1c36
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-14 11:18:46 +0300

    chore: add comments to explain JSX and component structure

--- react-lesson-01/src/App.jsx
+++ react-lesson-01/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css"; //here, we import things to use them in our component
 
 function App() {
-  const [count, setCount] = useState(0)
+  // we write JS codes here (to the field before "return")
+  const [count, setCount] = useState(0);
+  let a = 39;
+  const firstName = "Ali";
+  let names = ["Rumeysa", "Sumeyye", "Zeynep"];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    //we write HTML codes here
+    <div>
+      <p>value : {a}</p>
+      <p>customer name: {firstName} </p>
+      {/*while looping through the array, we need to give each element a unique key*/} {/*and we can style it like using style={{}} */}
+      {names.map((name, index) => (
+        <div style= {{
+          backgroundColor: 'orange',
+          border: '1px solid black'
+        }} key={index}>{name} </div>
+      ))}
+    </div>
+  ); //we use curly bracets to write JavaScript expressions inside JSX
 }
 
-export default App
+export default App; // we esport the "app" component from here so that it can be imported into main.js (so these codes will be included in the div in index.html)
--- react-lesson-01/src/main.jsx
+++ react-lesson-01/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' //we imported app.jsx here
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(//we selected the element whose id is root 
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)// the codes in the "app" component will be imported here (into the element whose id is root)
