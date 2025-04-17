821c4e120487f35ba286acca74b182adddb69e10
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-17 11:43:40 +0300

    feat: add example and explanations for deleting a data by using "delete"

--- lesson-07-axios/src/App.jsx
+++ lesson-07-axios/src/App.jsx
@@ -8,12 +8,12 @@ const BASE_URL = "http://localhost:3000";//we define main adress of API and we c
 function App() {
 
   const getAllUsers = async() => {
-    //axios is a tool we use to pull/get data from API
+    //  axios is a tool we use to pull/get data from API
   const response = await axios.get(BASE_URL + "/users");//sends request to /user's endpoint from API
   console.log(response.data)//it prints the data in the returned response to the terminal with console.log
   }
 
-  //in this function, userId comes as a parameter and is added to the URL.
+  //  in this function, userId comes as a parameter and is added to the URL.
   const getUserById = async(userId) => {
     const response = await axios.get(`${BASE_URL}/users/${userId}`); {/* BASE_URL + "/users" + userId*/}
     console.log(response.data)
@@ -28,6 +28,11 @@ function App() {
     await axios.put(`${BASE_URL}/users/${userId}`, updatedUser);//we use "put" to update a data
   }
 
+  const deleteUserById = async (userId) => {//we defined a parameter to use when we call this function
+    const deletedResponse = await axios.delete(`${BASE_URL}/users/${userId}`)//we use "delete" to delete a data
+    console.log(deletedResponse.data)
+  }
+
   //this code runs only once when the page loads because we added "[]" (empty dependency)
   useEffect(() => {
     // getAllUsers();//retrieves all users
@@ -38,13 +43,15 @@ function App() {
     //   "password" : "8798"
     // }//we created a new user, but we did not add id because local server will assign automatically
 
-    createUser(newUser);
+    // createUser(newUser);
 
-    //we defined new values by assigning parameters
-    updateUser("2", {
-      "username" : "cenk",
-      "password" : "7890"
-    })
+    // //we defined new values by assigning parameters
+    // updateUser("2", {
+    //   "username" : "cenk",
+    //   "password" : "7890"
+    // })
+
+    deleteUserById(1);//we assigned "1" as a parameter (user ID)
   }, [])
 
   return (
