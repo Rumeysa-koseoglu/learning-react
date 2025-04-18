150d0b2b56c5b4589cc97628f9b6db23573265e3
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-18 12:25:14 +0300

    feat: implement counter functionality with redux toolkit

--- lesson-08-redux-toolkit/src/App.jsx
+++ lesson-08-redux-toolkit/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import { useSelector } from 'react-redux'
+import { useState } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counterSlice"; //we imported decrement and increment functions from counterSlice.jxs
 
 function App() {
+  const dispatch = useDispatch();
 
-  const state = useSelector((store) => store.counter)
-  console.log(state)
+  const counter = useSelector((store) => store.counter); //we used useSelector to read store data in component
+  console.log(counter); //we access the initialState in counterSlice with (counter)
 
   return (
     <>
-
+      <div>
+        <div>{counter.value}</div>
+        <div>
+          <button onClick={() => dispatch(increment())}>increase</button> {/**we called increment function from the counterSlice reducers using dispatch */}
+          <button onClick={() => dispatch(decrement())}>decrease</button>{/**we called decrement function from counterSlice reducers using dispatch */}
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- lesson-08-redux-toolkit/src/redux/counterSlice.jsx
+++ lesson-08-redux-toolkit/src/redux/counterSlice.jsx
@@ -7,10 +7,16 @@ const initialState = {
 export const counterSlice = createSlice({
   name: "counter",
   initialState,
+  //we define functions in reducers
   reducers: {
-
+    increment: (state) => {
+      state.value = state.value + 1;
+    },
+    decrement: (state) => {
+      state.value = state.value - 1;
+    }
   },
 });
 
-export const {} = counterSlice.actions;
+export const { increment, decrement} = counterSlice.actions;//we export our functions
 export default counterSlice.reducer;
