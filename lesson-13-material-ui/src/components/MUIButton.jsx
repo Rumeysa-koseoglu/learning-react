efa340a5fdcd18b7fa852a982d8b30a2456ef446
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-04-26 11:56:24 +0300

    feat: add MUI buttons with icons and colors

---
+++ lesson-13-material-ui/src/components/MUIButton.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from "@mui/material";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+function MUIButton() {
+  return (
+    <div>
+      <div style={{marginBottom: "50px"}} >
+        <Button variant="text">Login</Button>
+        <Button variant="contained">Login</Button>
+        <Button variant="outlined">Login</Button>
+      </div>
+
+    <div style={{marginBottom: "50px"}} >
+        <Button color="primary">Sign in</Button>
+        <Button color="error">Sign in</Button>
+        <Button color="info">Sign in</Button>
+        <Button color="secondary">Sign in</Button>
+        <Button color="success" variant="contained">Sign in</Button>
+    </div>
+
+    <div style={{marginBottom: "50px"}} >
+        <Button size="small" variant="contained" color="warning" style={{marginRight: "10px"}}>Save</Button>
+        <Button size="medium" variant="contained" color="secondary" style={{marginRight: "10px"}}>Save</Button>
+        <Button size="large" variant="contained" color="secondary">Save</Button>
+    </div>
+
+    <div>
+        <Button size="medium" variant="contained" color="success" startIcon={<AddCircleIcon/>} style={{marginRight: "10px"}}>Save</Button>
+        <Button size="medium" variant="contained" color="success" endIcon={<AddCircleIcon/>} style={{marginRight: "10px"}}>Save</Button>
+    </div>
+
+    </div>
+  );
+}
+
+export default MUIButton;
