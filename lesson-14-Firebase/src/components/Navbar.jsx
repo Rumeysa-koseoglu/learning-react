882fb161ab831fa6f72b20a4a757bfb42755a8d4
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-07-10 14:40:15 +0300

    feat: initialize project with React Router and implement navbar, auth and home pages with styled UI

---
+++ lesson-14-Firebase/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-left">FIREBASE</div>
+      <div className="navbar-right">Log Out</div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ lesson-14-Firebase/src/pages/Auth.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/Auth.css";
+
+function Auth() {
+  return (
+    <div className="auth">
+      <h3 className="auth-header">Sign in / Sign up</h3>
+      <div className="input-div">
+        <input type="text" placeholder="email adress" />
+        <input type="password" placeholder="password" />
+      </div>
+      <div className="buttons-wrapper" style={{ marginTop: "20px" }}>
+        <button className="login-button">Log in</button>
+        <button className="register-button">Sign up</button>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
