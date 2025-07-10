111a3d603e6a948650ecf6588c51fb10d2dfda2a
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-07-10 22:57:50 +0300

    feat: set up Firebase auth with env config and implement user sign-up
    - Moved Firebase credentials to .env for security
    - Updated firebase.jsx to use environment variables
    - Added user sign-up functionality using Firebase Auth

--- lesson-14-Firebase/src/App.jsx
+++ lesson-14-Firebase/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import RouterConfig from "./config/RouterConfig";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
       <Navbar />
       <RouterConfig />
+      <ToastContainer position="top-right" autoClose={2000} />
     </div>
   );
 }
--- lesson-14-Firebase/src/pages/Auth.jsx
+++ lesson-14-Firebase/src/pages/Auth.jsx
@@ -1,21 +1,58 @@
 import React from "react";
 import "../css/Auth.css";
 import { FaGoogle } from "react-icons/fa";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "./Firebase";
 
 function Auth() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = response.user;
+      if (user) {
+        toast.success("user added");
+        setEmail("");
+        setPassword("");
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="auth">
       <h3 className="auth-header">Sign in / Sign up</h3>
       <div className="input-div">
-        <input type="text" placeholder="email adress" />
-        <input type="password" placeholder="password" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          placeholder="email adress"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="password"
+        />
       </div>
       <div className="buttons-wrapper" style={{ marginTop: "20px" }}>
         <button className="google-button">
           <FaGoogle style={{ marginRight: "10px" }} /> Sign in with Google
         </button>
         <button className="login-button">Log in</button>
-        <button className="register-button">Sign up</button>
+        <button onClick={register} className="register-button">
+          Sign up
+        </button>
       </div>
     </div>
   );
