aa8292ce7f50c01b814781bfda176dc1e4dfd2c0
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-07-11 19:48:46 +0300

    feat: implement user logout using Firebase Auth

--- lesson-14-Firebase/src/components/Navbar.jsx
+++ lesson-14-Firebase/src/components/Navbar.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import "../css/Navbar.css";
+import { signOut } from "firebase/auth";
+import { auth } from "../pages/Firebase";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    toast.success("logging out");
+    setTimeout(() => {
+      signOut(auth);
+      navigate("/auth");
+    }, 3000);
+  };
+
   return (
     <div className="navbar">
       <div className="navbar-left">FIREBASE</div>
-      <div className="navbar-right">Log Out</div>
+      <div onClick={logOut} className="navbar-right">
+        Log Out
+      </div>
     </div>
   );
 }
